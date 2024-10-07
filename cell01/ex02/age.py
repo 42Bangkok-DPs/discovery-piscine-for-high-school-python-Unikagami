bdf7a388d25d872a028d7b994d68390dcbcba88a
unikagami <uni_kagami@outlook.com>
2024-10-08 01:20:44 +0700

    -

---
+++ cell01/ex02/age.py
@@ -0,0 +1,4 @@
+age = 15
+you_age = 42
+my_age = age + you_age
+print(str(my_age) + "\n")
---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+name = input("what is your name : ")
+last_name = input("what is your last name : ")
+print("hello, " + name.strip() + " " + last_name.strip())
