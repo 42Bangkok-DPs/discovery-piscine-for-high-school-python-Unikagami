98e29355d5f81f9aae4e1aa336ea42d3c72a7990
unikagami <uni_kagami@outlook.com>
2024-10-07 23:44:09 +0700

    -

---
+++ cell00/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Ratjinan"
+last_name = "Chunhachavanwong"
+print(first_name + " " + last_name + "\n")
---
+++ cell00/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Ratjinan"
+last_name = "Chunhachavanwong"
+whole_name = (first_name + " " + last_name)
+print(whole_name + "\n")
