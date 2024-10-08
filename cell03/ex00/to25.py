78780ce63db935a9913ad3684ce0cad2db32f6e6
Unikagami <uni_kagami@outlook.com>
2024-10-08 15:26:03 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,6 @@
+number = int(input("enter the number less than 25 : "))
+if number > 25 :
+    print("Error")
+while number < 26 :
+    print("Inside the loop, my variable is" + " " + str(number))
+    number += 1
