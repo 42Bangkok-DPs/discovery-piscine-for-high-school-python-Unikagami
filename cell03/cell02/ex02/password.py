91845bab9e90dc5735374f3361e8430789c808cf
Unikagami <uni_kagami@outlook.com>
2024-10-08 16:01:59 +0700

    Add files via upload

---
+++ cell03/cell02/ex02/password.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+password = input("enter your password")
+if password == "python is awesome":
+    print("ACCESS GRANTED")
+else :
+    print("ACCESS DENIED")
---
+++ cell03/cell02/ex03/mult.py
@@ -0,0 +1,12 @@
+first_number = int(input("enter the first number : "))
+second_number = int(input("enter the second number : "))
+# multiply the variables
+result = first_number * second_number
+print(result)
+the_result = result
+if result <0 :
+    print("this number is negative")
+elif result ==0 :
+    print("this number is zero")
+else :
+    print("this number is positive")
