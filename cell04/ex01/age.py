a92eb24c0c4f8f4f791defd3451d205f87353f1f
Unikagami <uni_kagami@outlook.com>
2024-10-09 10:25:10 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+age = input("please tell me your age : ")
+print("you are currently " + age + " years old")
+converted_age = int(age)
+print("In 10 years, you'll be "+ str(converted_age + 10) + " years old")
+print("In 10 years, you'll be "+ str(converted_age + 20) + " years old")
+print("In 10 years, you'll be "+ str(converted_age + 30) + " years old")
